254ff1040fda272abce93098fbd618c87b91c0cd
AndrMe <124451118+AndrMe@users.noreply.github.com>
2023-02-20 19:50:37 +0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,16 @@
+from tkinter import*
+
+def click():
+    name=En.get()
+    l1.configure(text=f'Приветствую тебя,{name}, очень рад знакомству!')
+    
+root=Tk()
+root.geometry('400x400')
+root.title('Задание 10')
+En=Entry(bg='white',font='colsolas',width=10)
+l1=Label(bg='grey',fg='blue',width=60)
+button=Button(text='Приветствие',bg='grey',command=click)
+l1.place(relx=0,rely=0)
+En.place(relx=0.4,rely=0.2)
+button.place(relx=0.4,rely=0.5)
+root.mainloop()
